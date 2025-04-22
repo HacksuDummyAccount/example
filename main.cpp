5477d7045383cbb7e5ea08b7e14195c0566e252b
HacksuDummyAccount <warrioros2017@gmail.com>
2025-04-22 19:43:12 -0400

    beat noah to it

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <string>
+
 int main () {
-    int x = 3
+    int x = 3;
+    std::string output;
+    std::cout << "HELLO PLEASE PROVIDE INPUT ";
+    std::cin >> output;
+
+    std::cout << "YOUR OUTPUT: " << output + std::to_string(x) << endl; 
 }
